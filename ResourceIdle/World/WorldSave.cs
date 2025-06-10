27df1d4a7635c1a0a4bff63437e5a5c01c9c85d4
Joy Schnittker <dev@joyersch.de>
2025-06-10 18:05:04 +0200

    Added noise

--- ResourceIdle/World/WorldSave.cs
+++ ResourceIdle/World/WorldSave.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
+using System.Security.Cryptography;
 using Joyersch.Monogame.Storage;
 
 namespace ResourceIdle.World;
@@ -12,10 +13,22 @@ public class WorldSave : ISave
 
     public PlayerData PlayerData { get; set; } = GenerateNewPlayerDataSet();
 
+    public int WorldSeed { get; set; } = GenerateSeed();
+
     public void Reset()
     {
         PlayerData = GenerateNewPlayerDataSet();
         CaveData = [];
+    }
+
+    private static int GenerateSeed()
+    {
+        byte[] toHash = BitConverter.GetBytes(DateTime.Now.Ticks);
+        byte[] result = [];
+        using SHA256 sha = SHA256.Create();
+        result = sha.ComputeHash(toHash);
+
+        return BitConverter.ToInt32(result);
     }
 
     private static PlayerData GenerateNewPlayerDataSet()
